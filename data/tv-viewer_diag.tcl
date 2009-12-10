62ec72184eb2e2ce9f8eefd1e05883972eefeff8
Christian Rapp <christianrapp@users.sourceforge.net>
2009-12-10 18:08:25 +0100

    *Forgot to update build number and date for revision 39

--- data/tv-viewer_diag.tcl
+++ data/tv-viewer_diag.tcl
@@ -57,7 +57,7 @@ This is not recommended!"
 	}
 }
 
-set option(release_version) {0.8.1b3 38 09.12.2009}
+set option(release_version) {0.8.1b3 40 10.12.2009}
 
 # Start options for the program
 array set start_options {--version 0 --help 0 --debug 0}
--- tv-viewer_install.sh
+++ tv-viewer_install.sh
@@ -37,7 +37,7 @@ might not point to the correct location.
 exit 1
 }
 
-set option(release_version) {0.8.1b3 38 09.12.2009}
+set option(release_version) {0.8.1b3 40 10.12.2009}
 array set start_options {--uninstall 0 --target 0 --nodebug 0 --manpath 0 --nodepcheck 0}
 foreach command_argument $argv {
 	if {[string first = $command_argument] == -1 } {
